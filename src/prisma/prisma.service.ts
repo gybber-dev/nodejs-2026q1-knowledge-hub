2efc7d50347f3af3a4f5d6474e8eda9fdbd1bdf9
gybber <farscince@gmail.com>
2026-04-12 14:08:31 +0500

    impr: add prisma pool

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,8 @@
+import * as os from 'os';
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService
@@ -8,9 +10,15 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   constructor() {
-    const adapter = new PrismaPg({
+    const pool = new Pool({
       connectionString: process.env.DATABASE_URL,
+      max: process.env.DB_POOL_SIZE
+        ? parseInt(process.env.DB_POOL_SIZE, 10)
+        : os.cpus().length * 2 + 1,
+      idleTimeoutMillis: 30000,
+      connectionTimeoutMillis: 2000,
     });
+    const adapter = new PrismaPg(pool);
     super({ adapter });
   }
 
