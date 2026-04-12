e67c200ccbeee250994ee1cc2e8ea811d164bf11
gybber <farscince@gmail.com>
2026-04-12 12:37:10 +0500

    feat: create prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { PrismaModule } from './prisma/prisma.module';
 import { CategoryModule } from './category/category.module';
 import { ArticleModule } from './article/article.module';
 import { CommentModule } from './comment/comment.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CategoryModule, ArticleModule, CommentModule, UserModule],
+  imports: [
+    PrismaModule,
+    CategoryModule,
+    ArticleModule,
+    CommentModule,
+    UserModule,
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
